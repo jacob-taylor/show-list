4e2a594dfadd37212489b16acd2777cfd3f14437
jctaylor <jctaylor210@gmail.com>
2022-03-24 19:12:30 -0500

    Set up base home and settings screens and navigation between the two

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const SettingsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default SettingsScreen;

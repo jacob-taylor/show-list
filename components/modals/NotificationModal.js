252d85e6203e24871828800ae5bad7da73d57f8f
jctaylor <jctaylor210@gmail.com>
2022-05-01 10:28:51 -0500

    Created Notification Modal  check boxes

--- components/modals/NotificationModal.js
+++ components/modals/NotificationModal.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,55 +6,100 @@ import {
   TouchableOpacity,
   Dimensions,
   Modal,
-  Image,
-  ScrollView,
-  Linking,
+  Button,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import {
-  MOVIEDB_API_KEY,
-  MOVIEDB_API_URL,
-  MOVIEDB_POSTER_URL,
-  IMDB_URL,
-} from "../../constants";
 
 const screenWidth = Dimensions.get("screen").width;
 const screenHeight = Dimensions.get("screen").height;
 
 const NotificationModal = ({ modalVisible, setModalVisible }) => {
+  const [notifications, setNotifications] = useState({ on: true, off: false });
+  const pressHandler = (press) => {
+    if (press === "on") {
+      setNotifications({ yes: !notifications.on });
+    } else if (press === "off") {
+      setNotifications({ no: !notifications.off });
+    }
+  };
+
   return (
     <Modal
       visible={modalVisible}
       animationType="slide"
-      presentationStyle="pageSheet"
+      presentationStyle="overFullScreen"
+      transparent={true}
     >
-      <TouchableOpacity
-        onPress={() => {
-          setModalVisible(false);
-        }}
-        style={styles.closeBtn}
-      >
-        <Ionicons name="close" color="white" size={30} />
-      </TouchableOpacity>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text>Enable Push Notifications</Text>
+          <View style={styles.optionsView}>
+            <View style={styles.checkBox}>
+              <TouchableOpacity onPress={() => pressHandler("on")}>
+                <Ionicons
+                  name="checkmark"
+                  color={notifications.yes ? "black" : "white"}
+                  size={32}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text>ON</Text>
+          </View>
+          <View style={styles.optionsView}>
+            <View style={styles.checkBox}>
+              <TouchableOpacity onPress={() => pressHandler("off")}>
+                <Ionicons
+                  name="checkmark"
+                  color={notifications.no ? "black" : "white"}
+                  size={32}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text>OFF</Text>
+          </View>
+          <Button
+            title="Close"
+            onPress={() => {
+              setModalVisible(false);
+            }}
+          />
+        </View>
+      </View>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-  modalView: {
-    backgroundColor: "white",
-    padding: 35,
+  centeredView: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 50,
   },
-  container: {
-    flex: 1,
+  modalView: {
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    backgroundColor: "white",
+    height: screenHeight * 0.2,
+    width: screenWidth * 0.75,
+    borderColor: "black",
+    // borderWidth: 1,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.5,
+    elevation: 10,
+  },
+  optionsView: {
     flexDirection: "column",
     alignItems: "center",
+    justifyContent: "center",
   },
-  closeBtn: {
-    backgroundColor: "black",
-    borderRadius: 50,
-    alignSelf: "flex-start",
+  checkBox: {
+    borderRadius: 3,
+    borderWidth: 1,
+    marginRight: 10,
   },
 });
 

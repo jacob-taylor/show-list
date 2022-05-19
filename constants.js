1e71d26163b4ea90d2f37f869b50e68a218d8a49
Bryan Reed <bryan87reed@gmail.com>
2022-05-18 19:25:23 -0700

    Corrects the share url

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // * When running local API use "http://localhost:3000/api";
 // export const API_URL = "http://localhost:3000/api";
 export const API_URL = "https://the-list-now.herokuapp.com/api";
-// export const SHARE_URL = "https://localhost:300/share"
+// export const SHARE_URL = "https://localhost:300/share";
 export const SHARE_URL = "https://share.thelistnow.com/share";
 
 export const MOVIEDB_POSTER_URL = "https://image.tmdb.org/t/p/original";
